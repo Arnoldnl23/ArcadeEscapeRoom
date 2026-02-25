3cda4857f2881798d94d3b7faa91035b9275ed9b
Arnoldnl23 <arnoldnl@miamioh.edu>
2026-02-25 10:31:44 -0500

    Maze player now moves when button is pressed
    Also adds the library that allows testing without a headset

--- EscapeRoomProject/Assets/MazeAssets/MazeButtons.cs
+++ EscapeRoomProject/Assets/MazeAssets/MazeButtons.cs
@@ -1,49 +1,85 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class MazeButtons : MonoBehaviour
 {
-    private Transform movingPart;
-    private Vector3 pressDirection = Vector3.down;
-    private float pressDistance = 0.2f;
-    private Vector3 startPosition;
+    public Transform visualTarget;
+    private Transform pokeAttach;
+    private Vector3 initialPos;
+    private Vector3 offset;
+    private Vector3 localAxis = new Vector3(0,-1,0);
+
+    private bool freeze;
 
     private Transform mazePlayer;
     public Vector3 moveDirection;
 
     private XRBaseInteractable interactable;
+    private bool isFollowing = false;
     
     void Start()
     {
-        movingPart = GetComponent<Transform>();
-        startPosition = transform.position;
-
+        initialPos = visualTarget.localPosition;
         mazePlayer = GameObject.Find("MazePlayer").GetComponent<Transform>();
 
-        interactable = GetComponent<XRSimpleInteractable>();
+        interactable = GetComponent<XRBaseInteractable>();
         if (interactable != null)
         {
+            interactable.hoverEntered.AddListener(Follow);
             interactable.selectEntered.AddListener(OnSelectEntered);
-            interactable.selectExited.AddListener(OnSelectExited);
+            interactable.hoverExited.AddListener(Reset);
+        }
+    }
+
+    public void Follow (BaseInteractionEventArgs hover)
+    {
+        if (hover.interactorObject is XRPokeInteractor)
+        {
+            XRPokeInteractor interactor = (XRPokeInteractor)hover.interactorObject;
+            isFollowing = true;
+            pokeAttach = interactor.attachTransform;
+            offset = visualTarget.position - pokeAttach.position;
+            freeze = false;
+        }
+    }
+
+    public void Reset(BaseInteractionEventArgs hover)
+    {
+        if (hover.interactorObject is XRPokeInteractor)
+        {
+            isFollowing = false;
+            freeze = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (freeze)
+            mazePlayer.Translate(moveDirection * Time.deltaTime);
+
+        if (isFollowing)
+        {
+            Vector3 localTargetPosition = visualTarget.InverseTransformPoint(pokeAttach.position + offset);
+            Vector3 constrainedTargetPosition = Vector3.Project(localTargetPosition, localAxis);
+            visualTarget.position = visualTarget.TransformPoint(constrainedTargetPosition);
+        }
+        else
+        {
+            visualTarget.localPosition = initialPos;
         }
     }
 
     // This method is called when an interactor starts hovering or selecting
-    void OnSelectEntered(SelectEnterEventArgs args)
+    void OnSelectEntered(BaseInteractionEventArgs args)
     {
-
+        freeze = true;
         Debug.Log("Button Pressed!");
-        movingPart.localPosition = startPosition + (pressDirection * pressDistance);
         
         // Trigger movement based on which button it is
-        mazePlayer.Translate(moveDirection * Time.deltaTime);
+
     }
 
-    // This method is called when the interactor stops selecting
-    void OnSelectExited(SelectExitEventArgs args)
-    {
-        // Return button to original position
-        movingPart.localPosition = startPosition;
-    }
+
 }
