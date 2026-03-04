6ee22cd7720f64bcde99ce09c7589cf19260938e
Arnoldnl23 <arnoldnl@miamioh.edu>
2026-03-04 16:42:00 -0500

    Small bug fixes on maze, added prefabs

--- EscapeRoomProject/Assets/MazeAssets/MazePlayer.cs
+++ EscapeRoomProject/Assets/MazeAssets/MazePlayer.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public bool isColliding = false;
+    public Canvas mazeResult;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -13,9 +14,10 @@ public class Player : MonoBehaviour
         if (collision.gameObject.tag == "Goal")
         {
             //Spawn in gun and text saying to take the gun
-            Instantiate(Resources.Load<GameObject>("Cosmic_Retro_Blaster_3_9"), new Vector3(9.04300022f, 1.83099997f, -6.62900019f), Quaternion.identity);
+            Instantiate(Resources.Load<GameObject>("Cosmic_Retro_Blaster_3_9"), new Vector3(8.80944157f, 2.00699997f, -7.03487587f), new Quaternion(0, 0.707106829f, 0, 0.707106829f));
             GameManager.tickets += 10;
 
+            mazeResult.GetComponent<CanvasGroup>().alpha = 1;
             this.gameObject.SetActive(false);
             this.enabled = false;
         }
