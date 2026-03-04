3881a86f3baa913eacb099fa74420262b1a55a64
Arnoldnl23 <arnoldnl@miamioh.edu>
2026-03-04 13:58:35 -0500

    Maze spawns blaster, was moved up, blaster prefab made

--- EscapeRoomProject/Assets/MazeAssets/MazePlayer.cs
+++ EscapeRoomProject/Assets/MazeAssets/MazePlayer.cs
@@ -10,6 +10,15 @@ public class Player : MonoBehaviour
         {
             isColliding = true;
         }
+        if (collision.gameObject.tag == "Goal")
+        {
+            //Spawn in gun and text saying to take the gun
+            Instantiate(Resources.Load<GameObject>("Cosmic_Retro_Blaster_3_9"), new Vector3(9.04300022f, 1.83099997f, -6.62900019f), Quaternion.identity);
+            GameManager.tickets += 10;
+
+            this.gameObject.SetActive(false);
+            this.enabled = false;
+        }
     }
 
     private void OnCollisionStay(Collision collision)
