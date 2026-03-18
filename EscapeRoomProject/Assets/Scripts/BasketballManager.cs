79b9d0b5e99ba5fb48d0c154c0f72e8211122564
Arnoldnl23 <arnoldnl@miamioh.edu>
2026-03-18 10:53:51 -0400

    small bug fixes and rigid body stuff

--- EscapeRoomProject/Assets/Scripts/BasketballManager.cs
+++ EscapeRoomProject/Assets/Scripts/BasketballManager.cs
@@ -1,13 +1,18 @@
+using Mono.Cecil;
 using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BasketballManager : MonoBehaviour
 {
-    public List<int> correctSeries = new List<int>{ 0, 1, 1, 0 };
-    private List<int> currentSeries = new List<int> { 0, 1, 1 };
+    private List<int> correctSeries = new List<int>{ 0, 1, 0, 0, 1 };
+    private List<int> currentSeries = new List<int> {};
+
+    private bool beenCompleted = false;
 
     private AudioSource audioSource;
+    public AudioClip correctSound;
+    public AudioClip incorrectSound;
 
     private void Start()
     {
@@ -16,23 +21,29 @@ public class BasketballManager : MonoBehaviour
 
     public void checkResult(int value)
     {
-        currentSeries.Add(value);
-        if (currentSeries.Count == 4)
+        if (!beenCompleted)
         {
-            for (int i = 0; i < currentSeries.Count; i++) { 
-                if (currentSeries[i] != correctSeries[i])
+            currentSeries.Add(value);
+            if (currentSeries.Count == 4)
+            {
+                for (int i = 0; i < currentSeries.Count; i++)
                 {
-                    currentSeries.Clear();
-                    audioSource.Play();
-                    return;
+                    if (currentSeries[i] != correctSeries[i])
+                    {
+                        currentSeries.Clear();
+                        audioSource.resource = incorrectSound;
+                        audioSource.Play();
+                        return;
+                    }
                 }
-            }
 
-            Instantiate(Resources.Load<GameObject>("Crowbar"), new Vector3(25.1299992f, 0.867999971f, -11.8599997f), new Quaternion(0, 0, 0, 0));
-            audioSource.Play();
-            GameManager.progressCount++;
-            this.enabled = false;
-            
+                Instantiate(Resources.Load<GameObject>("Crowbar"), new Vector3(25.1299992f, 0.867999971f, -11.8599997f), new Quaternion(0, 0, 0, 0));
+                audioSource.resource = correctSound;
+                audioSource.Play();
+                FindFirstObjectByType<GameManager>().updateTickets(10);
+                GameManager.progressCount++;
+                beenCompleted = true;
+            }
         }
     }
 }
